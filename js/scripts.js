cc178c9cf3cd810c940712d1d99556dfca5f5441
J Mieras <jmieras@gmail.com>
2020-05-20 01:53:54 -0700

    commit recent changes to scripts.js before extended break

--- js/scripts.js
+++ js/scripts.js
@@ -15,18 +15,36 @@ const robogerArray = function(inputtedNumber, inputName) {
     if (functionArray[i]%3 === 0) {
       midArray.push ("Won't you be my neighbor, " + inputName + "?"); 
       }
-      // // else if (functionArray[i] === three ) {
-      // //   endArray.push ("Won't you be my neighbor?");
-      // //   }
-      //   else if (functionArray[i] === two ) {
-      //     endArray.push ("Boop!");
-      //     }
-      //   //   else if (functionArray[i]===(one)) {
-      //   //     midArray.push ("Beep!");
-      //   //     }
-  else {
-        midArray.push(i);
-      }
+    else {
+    midArray.push(i);
+  }
+
+  // const threeArray = midArray.forEach(
+  //   if (functionArray[i] === three ) {
+  //       endArray.push ("Won't you be my neighbor?");
+  //       }
+  //           else if (functionArray[i] === two ) {
+  //     endArray.push ("Boop!");
+  //     });
+
+
+
+  // for (let i=1; i<=inputtedNumber; i+=1) {
+  //   if (functionArray[i]%3 === 0) {
+  //     midArray.push ("Won't you be my neighbor, " + inputName + "?"); 
+  //     }
+  //     // // else if (functionArray[i] === three ) {
+  //     // //   endArray.push ("Won't you be my neighbor?");
+  //     // //   }
+  //     //   else if (functionArray[i] === two ) {
+  //     //     endArray.push ("Boop!");
+  //     //     }
+  //     //   //   else if (functionArray[i]===(one)) {
+  //     //   //     midArray.push ("Beep!");
+  //     //   //     }
+  // else {
+  //       midArray.push(i);
+  //     }
 
   const convertToString = midArray.toString();
   const threeString = (convertToString.replace(three, "Won't you be my neighbor?"));
