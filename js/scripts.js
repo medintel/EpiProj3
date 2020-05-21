66c6caee790ce84cd03eafc6290b7e5f88a9ce26
J Mieras <jmieras@gmail.com>
2020-05-20 23:09:39 -0700

    Adds functionality of scripts.js to properly parse and return string per specs

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,13 @@ let endArray = [];
 const robogerArray = function(inputtedNumber, inputName) {
   let functionArray= [];
   let midArray= [0];
+  let lateArray= [0];
   let three = /3/g;
+  // const tthree = three.find(Element => Element)
+  // const ttwo = RegExp('*2*','g')
   let two = /2/g;
   let one = /1/g;
+  let stringConvert = "0";
 
   for (let i=0; i<=inputtedNumber; i+=1) {
     functionArray.push(i);
@@ -13,11 +17,87 @@ const robogerArray = function(inputtedNumber, inputName) {
 
   for (let i=1; i<=inputtedNumber; i+=1) {
     if (functionArray[i]%3 === 0) {
-      midArray.push ("Won't you be my neighbor, " + inputName + "?"); 
+      midArray.push ("Won't you be my neighbor, " + inputName + "?");
+        }
+      else {
+        midArray.push(i);
+       } 
+    }
+    
+  midArray.forEach(function(item) {
+    if (Number.isNaN (item)) {
+      endArray.push(item);
+    }
+    else if (Number.isInteger(item)) {
+      stringConvert= item.toString();
+      if (stringConvert.includes('3')) {
+        endArray.push("Won't you be my neighbor?");
+      }
+        else if (stringConvert.includes('2')) {
+        endArray.push("Boop!");
+        }
+        else if (stringConvert.includes('1')){
+        endArray.push("Beep!");
+        }
+      else {
+        endArray.push(item);
+        }
       }
     else {
-    midArray.push(i);
-  }
+      endArray.push(item);
+      }
+  });
+
+
+
+ 
+  //     )
+
+  //     for (let index=0; index, i.length; )
+  //        //   stringConvert.replace(two, "Boop!")
+  //   //   endArray.push(i);
+  //   }
+  // }
+
+  //   else {
+  //     for (let index=1; index<=i; index+=1) {
+  //       if (functionArray[i] === 2) {
+  //           endArray.push ("Boop!");
+  //         }
+  //       else if (functionArray[i] === 1){
+  //           endArray.push ("Beep!");
+  //       }
+
+    // else if (functionArray[i] === 2){
+    //   endArray.push ("Boop!");
+    // }
+    // else if (functionArray[i] === 1){
+    //   endArray.push ("Beep!");
+    // }
+
+
+  // for (let i=1; i<=inputtedNumber; i+=1) {
+    // midArray.forEach(function(i) {
+    //   stringConvert.replace(two, "Boop!")
+    //   endArray.push(i);
+    // });
+
+    // stringConvert.replace(three, "Won't you be my neighbor?");
+    // stringConvert.replace(two, "Boop!"));
+
+    // if (stringConvert.includes(three)) {
+    //       endArray.push ("Won't you be my neighbor?");
+    //     }
+    //   else if (stringConvert.includes(two)) {
+    //     endArray.push ("Boop!");
+    //     }
+    //     else if (stringConvert.includes(one)) {
+    //       endArray.push ("Beep!");
+    //       }
+    //       else {
+    //         endArray.push (midArray[i]);
+    //         }
+    // }
 
   // const threeArray = midArray.forEach(
   //   if (functionArray[i] === three ) {
@@ -31,7 +111,7 @@ const robogerArray = function(inputtedNumber, inputName) {
 
   // for (let i=1; i<=inputtedNumber; i+=1) {
   //   if (functionArray[i]%3 === 0) {
-  //     midArray.push ("Won't you be my neighbor, " + inputName + "?"); 
+  //     midArray.push ("Won't you be my neighbor, " + inputName + "?");
   //     }
   //     // // else if (functionArray[i] === three ) {
   //     // //   endArray.push ("Won't you be my neighbor?");
@@ -43,14 +123,14 @@ const robogerArray = function(inputtedNumber, inputName) {
   //     //   //     midArray.push ("Beep!");
   //     //   //     }
   // else {
-  //       midArray.push(i);
+  //       endArray.push(i);
   //     }
 
-  const convertToString = midArray.toString();
-  const threeString = (convertToString.replace(three, "Won't you be my neighbor?"));
-  const twoString = (threeString.replace(two, "Boop!"));
-  endArray = (twoString.replace(one, "Beep!"));
-}
+  // const convertToString = midArray.toString();
+  // const threeString = (convertToString.replace(three, "Won't you be my neighbor?"));
+  // const twoString = (threeString.replace(two, "Boop!"));
+  // endArray = (twoString.replace(one, "Beep!"));
+
 return endArray;
 };
 
@@ -63,10 +143,10 @@ $(document).ready(function() {
     $(".userName").text(inputName).val();
 
     const inputtedNumber = parseInt($("#inputNumber").val());
-    
+
     const returnedArray = robogerArray(inputtedNumber, inputName);
     const finalString = returnedArray.toString();
-     $(".finalString").text(finalString).val();    
+     $(".finalString").text(finalString).val();
      alert(finalString);
 
     $("#results").show();
